e9868fc4ca30802be4c84b066f0a2c8e14898e30
Manegfx <manelgomez.art@gmail.com>
2025-07-14 18:57:08 +0200

    Add the SFX
    Add sfx to the slider when its value reaches 1 (once), and edit the script so it will happen.

--- tech-art-test-main/Assets/maanetorn/Farm Game UI - Simple 2D UI/Assets/Scripts/UI/CustomSlider.cs
+++ tech-art-test-main/Assets/maanetorn/Farm Game UI - Simple 2D UI/Assets/Scripts/UI/CustomSlider.cs
@@ -8,10 +8,14 @@ public class CustomSlider : MonoBehaviour
     public Slider slider;
     public float lerpSpeed = 5; 
     private float targetValue;
+    public AudioSource audioSource;
+    private bool sfxactivated = false;
+
     // Start is called before the first frame update
     void Start()
     {
         targetValue = slider.value;
+        
     }
 
     // Update is called once per frame
@@ -20,10 +24,24 @@ public class CustomSlider : MonoBehaviour
         if (slider.value != targetValue)
         {
             slider.value = Mathf.Lerp(slider.value, targetValue, Time.deltaTime * lerpSpeed);
+            if (Mathf.Abs(slider.value - targetValue) <= 0.01f)
+            {
+                slider.value = targetValue;
+            }
+        }
+        if(slider.value == 1 && sfxactivated == false)
+        {
+            sfxactivated = true;
+            PlaySFX();
         }
     }
     public void SetSliderValue(float value)
     {
         targetValue = value;
+    }
+
+    public void PlaySFX()
+    {
+        audioSource.Play();
     }
 }
